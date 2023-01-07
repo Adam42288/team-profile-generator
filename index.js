14b7fd0c320b397e6e0afff6039aaff28a3e9944
Adam <asantillana@apple.com>
2023-01-07 15:19:33 -0600

    added code for index.js and questions array

---
+++ index.js
@@ -0,0 +1,78 @@
+const inquirer = require('inquirer');
+
+
+const managerQuestions = [
+    {
+        name: "name",
+        message: "Enter your name",
+        type: "input",
+    },
+    {
+        name: "id",
+        message: "Enter your employee ID",
+        type: "number",
+    },
+    {
+        name: "email",
+        message: "Enter your email address",
+        type: "input",
+    },
+    {
+        name: "officeNumber",
+        message: "Enter your office number",
+        type: "number",
+    }
+];
+const engineerQuestions = [
+    {
+        name: "name",
+        message: "Enter your name",
+        type: "input",
+    },
+    {
+        name: "id",
+        message: "Enter your employee ID",
+        type: "number",
+    },
+    {
+        name: "email",
+        message: "Enter your email address",
+        type: "input",
+    },
+    {
+        name: "github",
+        message: "Enter your GitHub username",
+        type: "input",
+    }
+];
+const internQuestions = [
+    {
+        name: "name",
+        message: "Enter your name",
+        type: "input",
+    },
+    {
+        name: "id",
+        message: "Enter your employee ID",
+        type: "number",
+    },
+    {
+        name: "email",
+        message: "Enter your email address",
+        type: "input",
+    },
+    {
+        name: "school",
+        message: "Enter your School",
+        type: "input",
+    }
+];
+
+inquirer
+  .prompt(managerQuestions)
+  .then((response) => console.log('Success!'));
+
+
+function init() {}
+
+init();
