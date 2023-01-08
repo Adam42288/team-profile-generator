e946eb96270fd123782adce90db7f3f77078c789
Adam <asantillana@apple.com>
2023-01-08 13:36:29 -0600

    added css styling to project

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -118,17 +118,17 @@ generateHTML = (data) => {
         <body>
             <header>
                 <nav class="navbar" id="navbar">
-                    <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+                    <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Roster</span>
                 </nav>
             </header>
             <main>
-                <div class="container">
-                    <div class="row justify-content-center" id="team-cards">
-                        <!--Team Cards-->
-                        ${employeeCards}
-                    </div>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    <!--Team Cards-->
+                    ${employeeCards}
                 </div>
-            </main>
+            </div>
+        </main>
             
         </body>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
