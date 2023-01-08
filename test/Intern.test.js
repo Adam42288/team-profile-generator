c8b1629f460ac6d2f12cf1348a36c04b62e32098
Adam <asantillana@apple.com>
2023-01-08 13:52:10 -0600

    Added js code for intern test

---
+++ test/Intern.test.js
@@ -0,0 +1,21 @@
+// import Intern constructor
+const { default: test } = require('node:test');
+const Intern = require('../lib/Intern');
+
+// create an Intern object
+test('Create an Intern object', () => {
+    const intern = new Intern('Adam', 2832, 'asantillana@gmail.com', 'PSU');
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+// Get the School from getSchool()
+test('gets the school', () => {
+    const intern = new Intern('Adam', 2832, 'asantillana@gmail.com', 'PSU');
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+// Get the Role from getRole()
+test('gets Intern Role', () => {
+    const intern = new Intern('Adam', 2832, 'asantillana@gmail.com', 'PSU');
+    expect(intern.getRole()).toEqual('Intern');
+});
