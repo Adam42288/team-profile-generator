d0b998d45c1346285f908b934e7f03ad30d3b812
Adam <asantillana@apple.com>
2023-01-08 13:49:40 -0600

    added js code for engineer test

---
+++ test/Engineer.test.js
@@ -0,0 +1,21 @@
+// import Engineer constructor
+const { default: test } = require('node:test');
+const Engineer = require('../lib/Engineer');
+
+// create an Engineer object
+test('Create an Engineer object', () => {
+    const engineer = new Engineer('Adam', 2832, 'asantillana@gmail.com', 'adam42288');
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+// Get the Github from getGithub()
+test('gets Github account', () => {
+    const engineer = new Engineer('Adam', 2832, 'asantillana@gmail.com', 'adam42288');
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// Get the Role from getRole()
+test('gets Engineer Role', () => {
+    const engineer = new Engineer('Adam', 2832, 'asantillana@gmail.com', 'adam42288');
+    expect(engineer.getRole()).toEqual('Engineer');
+});
